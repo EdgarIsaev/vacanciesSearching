f4e4ae998e464409cb4b10cd9a2b5695da7077d3
isaev_kdi@mail.ru <isaev_kdi@mail.ru>
2023-11-25 11:05:12 +0400

    Initial Commit

---
+++ vacanciesSearching/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vacanciesSearching
+//
+//  Created by Эдгар Исаев on 25.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
